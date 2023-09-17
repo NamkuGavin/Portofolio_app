5416134e7b054e17a8b8b1a872214f7a5436e8fd
AJI Suryawan <ajisuryawan123@gmail.com>
2023-09-17 12:02:51 +0700

    feat: [GA detail work list]

--- lib/model/work_model.dart
+++
@@ -1,9 +0,0 @@
-class WorkModel {
-  String image;
-  String title;
-  String year;
-  String type;
-  String contain;
-
-  WorkModel(this.image, this.title, this.year, this.type, this.contain);
-}
--- lib/widget/work_item.dart
+++ lib/widget/work_item.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:my_portofolio/model/work_model.dart';
+import 'package:my_portofolio/page/detail_work_page.dart';
 
 class WorkItem extends StatelessWidget {
   final List<QueryDocumentSnapshot<Object?>> data;
@@ -11,71 +11,93 @@ class WorkItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(bottom: 20),
-          child: Container(
-            height: 250,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: NetworkImage(data[index]['image']),
-                    fit: BoxFit.cover),
-                borderRadius: const BorderRadius.all(Radius.circular(12))),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            PageRouteBuilder(
+              pageBuilder: (context, animation, secondaryAnimation) =>
+                  DetailWorkPage(detailData: data[index]),
+              transitionsBuilder:
+                  (context, animation, secondaryAnimation, child) {
+                const begin = Offset(-1.0, 0.0);
+                const end = Offset.zero;
+                const curve = Curves.easeInOut;
+                var tween = Tween(begin: begin, end: end)
+                    .chain(CurveTween(curve: curve));
+                var offsetAnimation = animation.drive(tween);
+                return SlideTransition(position: offsetAnimation, child: child);
+              },
+            ));
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: Container(
+              height: 250,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(data[index]['image']),
+                      fit: BoxFit.cover),
+                  borderRadius: const BorderRadius.all(Radius.circular(12))),
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 10),
-          child: Text(
-            data[index]['title'],
-            style: GoogleFonts.heebo(fontWeight: FontWeight.bold, fontSize: 22),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Text(
+              data[index]['title'],
+              style:
+                  GoogleFonts.heebo(fontWeight: FontWeight.bold, fontSize: 22),
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 20),
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 25),
-                child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 15, vertical: 3),
-                  decoration: const BoxDecoration(
-                    color: Color(0xFF142850),
-                    borderRadius: BorderRadius.all(Radius.circular(50)),
-                  ),
-                  child: Text(
-                    data[index]['year'],
-                    style: GoogleFonts.heebo(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 25),
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 15, vertical: 3),
+                    decoration: const BoxDecoration(
+                      color: Color(0xFF142850),
+                      borderRadius: BorderRadius.all(Radius.circular(50)),
+                    ),
+                    child: Text(
+                      data[index]['year'],
+                      style: GoogleFonts.heebo(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15),
+                    ),
                   ),
                 ),
-              ),
-              Text(
-                data[index]['type'],
-                style: GoogleFonts.heebo(
-                    color: const Color(0xFF8695A4),
-                    fontWeight: FontWeight.w400,
-                    fontSize: 15),
-              ),
-            ],
+                Text(
+                  data[index]['type'],
+                  style: GoogleFonts.heebo(
+                      color: const Color(0xFF8695A4),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 15),
+                ),
+              ],
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 15),
-          child: Text(
-            data[index]['contain'],
-            style: GoogleFonts.heebo(fontWeight: FontWeight.w400, fontSize: 15),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 15),
+            child: Text(
+              data[index]['contain'],
+              style:
+                  GoogleFonts.heebo(fontWeight: FontWeight.w400, fontSize: 15),
+            ),
           ),
-        ),
-        const Padding(
-          padding: EdgeInsets.only(bottom: 25),
-          child: IntrinsicHeight(child: Divider(color: Color(0xFFE0E0E0))),
-        ),
-      ],
+          const Padding(
+            padding: EdgeInsets.only(bottom: 25),
+            child: IntrinsicHeight(child: Divider(color: Color(0xFFE0E0E0))),
+          ),
+        ],
+      ),
     );
   }
 }
